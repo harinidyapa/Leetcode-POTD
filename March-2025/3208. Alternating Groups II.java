f4f57462b93ec7b054bd3b0b2ffdfb5686887a1e
harinidyapa <harinidyapa@gmail.com>
2025-03-09 22:21:32 +0530

    Create 3208. Alternating Groups II.java

---
+++ March-2025/3208. Alternating Groups II.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int numberOfAlternatingGroups(int[] colors, int k) 
+    {
+        int n=colors.length;
+        int left=0;
+        int res=0;
+        for(int right=1;right<(n-k+1);right++)
+        {
+            if(colors[right%n]==colors[(right-1)%n])
+            {
+                left=right;
+            }
+            if(right-left+1==k)
+            {
+                res++;
+                left++;
+            }
+        }
+        return res;
+    }
+}
